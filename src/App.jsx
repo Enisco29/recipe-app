344687b12f86417c558254f60595fb705f598f18
Enisco <opeyemioluwafisayo29@gmail.com>
2024-12-24 00:03:10 +0100

    ui design completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Route, Routes} from 'react-router-dom'
+import Sidebar from "./Components/Sidebar";
+import HomePage from './Pages/HomePage'
+import FavoritesPage from './Pages/FavoritesPage';
+
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
